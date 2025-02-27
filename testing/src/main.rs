38b124511dcf938a9ad213ee1fc4b2e14cddefdb
noah <n.gampper@outlook.de>
2025-02-27 23:47:58 +0100

    removed motor tests

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,8 +1,9 @@
-use mctrl::motor;
+//use mctrl::motor;
 
 fn main() {
-    let mut m1 = motor::Mtr::new(true, 5, 6, 13).unwrap();
-    m1.enable_motor();
-    m1.move_steps(200, true, 600000);
+    //let mut m1 = motor::Mtr::new(true, 5, 6, 13).unwrap();
+    //m1.enable_motor();
+    //m1.move_steps(200, true, 600000);
+    println!("hi")
 
 }
