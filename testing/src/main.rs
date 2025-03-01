e250adff0dba27141225f22a3fb72a2afa72dc0a
noah <n.gampper@outlook.de>
2025-03-01 10:28:49 +0100

    tested max motorspeeds

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -3,13 +3,13 @@ use mctrl::*;
 fn main() {
     let mut m1 = motor::Mtr::new(true, 5, 6, 13).unwrap();
     m1.enable_motor();
-    m1.move_steps(600, true, 3.0).unwrap();
+    m1.move_steps(800, true, 1.0).unwrap();
     //m1.move_steps(500, false, 2.0).unwrap();
     m1.disable_motor();
 
     let mut m2 = motor::Mtr::new(true, 23, 24, 25).unwrap();
     m2.enable_motor();
-    m2.move_steps(2000, true, 2.0).unwrap();
+    m2.move_steps(800, true, 1.0).unwrap();
     //m2.move_steps(500, false, 2.0).unwrap();
     m2.disable_motor();
 
