abafd03a7561f977e2696c081da4512620a4e8fe
noah <n.gampper@outlook.de>
2025-03-02 21:14:01 +0100

    decided to include pathfinding in position lib

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,20 +1,25 @@
 use mctrl::*;
 
 fn main() {
-    let mut m1 = motor::Mtr::new(true, 5, 6, 13).unwrap();
+    /*let mut m1 = motor::Mtr::new(true, 5, 6, 13).unwrap();
     m1.enable_motor();
     m1.move_steps(800, true, 1.5).unwrap();
     //m1.move_steps(500, false, 2.0).unwrap();
-    m1.disable_motor();
+    m1.disable_motor();*/
 
     let mut m2 = motor::Mtr::new(true, 23, 24, 25).unwrap();
     m2.enable_motor();
-    m2.move_steps(800, true, 1.5).unwrap();
-    //m2.move_steps(500, false, 2.0).unwrap();
+    m2.move_steps(800, true, 4.0).unwrap();
+    delay::delayms(200);
+    m2.move_steps(400, false, 2.0).unwrap();
+    delay::delayms(200);
+    m2.move_steps(800, true, 6.0).unwrap();
+    delay::delayms(200);
+    m2.move_steps(1200, false, 5.0).unwrap();
     m2.disable_motor();
 
-    let mut magnet = magnet::Magnet::new(26).unwrap();
+    /*let mut magnet = magnet::Magnet::new(26).unwrap();
     magnet.on();
     delay::delayms(3000);
-    magnet.off();
+    magnet.off();*/
 }
