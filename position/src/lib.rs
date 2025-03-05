71c7aef7dc7503e713d444c69085447247e524e5
noah <n.gampper@outlook.de>
2025-03-05 02:22:31 +0100

    added functionality to position for beaten pieces

--- position/src/lib.rs
+++ position/src/lib.rs
@@ -1,10 +1,11 @@
 pub mod position {
 
-    use std::num::ParseIntError;
+    use std::{collections::HashMap, num::ParseIntError};
 
 
     #[derive(Debug)]
     #[derive(Clone, Copy)]
+    #[derive(Eq, Hash, PartialEq)]
     pub enum Piece {
         King(bool),
         Queen(bool),
@@ -57,6 +58,24 @@ pub mod position {
 
 
     impl Piece {
+
+        fn from_char(chr: char) -> Option<Self> {
+            match chr {
+                'K' => Some(Piece::King(true)),
+                'Q' => Some(Piece::Queen(true)),
+                'R' => Some(Piece::Rook(true)),
+                'N' => Some(Piece::Knight(true)),
+                'B' => Some(Piece::Bishop(true)),
+                'P' => Some(Piece::Pawn(true)),
+                'k' => Some(Piece::King(false)),
+                'q' => Some(Piece::Queen(false)),
+                'r' => Some(Piece::Rook(false)),
+                'n' => Some(Piece::Knight(false)),
+                'b' => Some(Piece::Bishop(false)),
+                'p' => Some(Piece::Pawn(false)),
+                _ => None
+            }
+        }
 
         fn piece_to_letter(&self) -> &str {
             match &self {
@@ -150,13 +169,15 @@ pub mod position {
 
     #[derive(Debug)]
     pub enum MoveError {
+        FenParse(ParseIntError),
         NoFigurStart,
         OwnFigurEnd,
         MoveNotFitPiece(Piece),
         MoveParse(ParseIntError),
         WrongFigurStart,
         UnrightCoordinates,
-        UnallowedRochade
+        UnallowedRochade,
+        ImpossiblePosition
     }
 
     #[derive(Debug)]
@@ -164,6 +185,11 @@ pub mod position {
         ImpossibleMove(MoveError),
         MoveError(MoveError),
         Other
+    }
+
+    pub enum CleaningError {
+        KingBeforeQueen,
+        ImpossiblePosition
     }
 
     #[derive(Debug)]
@@ -192,15 +218,218 @@ pub mod position {
                     [Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None],
                     [Piece::None,Piece::None,Piece::None,Piece::Pawn(true),Piece::Pawn(true),Piece::Pawn(true),Piece::Pawn(true),Piece::Pawn(true),Piece::Pawn(true),Piece::Pawn(true),Piece::Pawn(true),Piece::None,Piece::None,Piece::None],
                     [Piece::None,Piece::None,Piece::None,Piece::Rook(true),Piece::Knight(true),Piece::Bishop(true),Piece::Queen(true),Piece::King(true),Piece::Bishop(true),Piece::Knight(true),Piece::Rook(true),Piece::None,Piece::None,Piece::None]],
-                moves: 1,
+                moves: 0,
                 en_passant: "-".to_string(),
                 rochade: [Piece::King(true),Piece::Queen(true),Piece::King(false),Piece::Queen(false)],
                 since_pawn_major: 0
             }
         }
 
-        pub fn from_fen(fen: &str) -> Self {
-            Position::new_reset()
+        pub fn from_fen(fen: &str) -> Result<Self, MoveError> {
+            let fen: Vec<&str> = fen.split_ascii_whitespace().collect();
+            let colorw = match fen[1] {
+                "w" => true,
+                _ => false
+            };
+            let moves = match fen[5].to_string().parse::<u32>() {
+                Ok(num) => {
+                    if colorw {
+                        (num-1)*2
+                    } else {
+                        (num-1)*2 + 1
+                    }
+                },
+                Err(rr) => return Err(MoveError::FenParse(rr))
+            };
+            let en_passant = fen[3].to_string();
+            let mut rochade = [Piece::None; 4];
+            fen[2].chars().for_each(|l| {
+                match l {
+                    'K' => rochade[0] = Piece::King(true),
+                    'Q' => rochade[1] = Piece::Queen(true),
+                    'k' => rochade[2] = Piece::King(false),
+                    'q' => rochade[3] = Piece::Queen(false),
+                    _ => {}
+                }
+            });
+            let since_pawn_major = match fen[4].parse::<u32>() {
+                Ok(num) => num,
+                Err(rr) => return Err(MoveError::FenParse(rr))
+            };
+            let mut all_pieces: HashMap<Piece, u8> = HashMap::new();
+            for v in [true, false] {
+                all_pieces.insert(Piece::King(v), 1);
+                all_pieces.insert(Piece::Queen(v), 1);
+                all_pieces.insert(Piece::Rook(v), 2);
+                all_pieces.insert(Piece::Knight(v), 2);
+                all_pieces.insert(Piece::Bishop(v), 2);
+                all_pieces.insert(Piece::Pawn(v), 8);
+            };
+            let mut fields = [[Piece::None; 14]; 8];
+            let rows: Vec<&str> = fen[0].split("/").collect();
+            let mut i = 0;
+            for row in rows {
+                let row: Vec<char> = row.chars().collect();
+                let mut j = 3;
+                for chr in row {
+                    if let Some(pce) = Piece::from_char(chr) {
+                        *(all_pieces.entry(pce).or_insert(0)) -= 1;
+                        fields[i][j] = pce;
+                        j += 1;
+                    } else {
+                        match chr.to_string().parse::<usize>() {
+                            Ok(num) => j += num,
+                            Err(rr) => return Err(MoveError::FenParse(rr))
+                        };
+                    }
+                };
+                i += 1;
+            }
+            let mut pos = Position { colorw, fields, moves, en_passant, rochade, since_pawn_major};
+            let mut scnd_run = all_pieces.clone();
+            let mut rerun = false;
+            for (k, v) in all_pieces.iter() {
+                for _ in 0..*v {
+                    match pos.add_rest(k.clone()) {
+                        Ok(_) => *(scnd_run.entry(*k).or_insert(*v)) -= 1,
+                        Err(_) => rerun = true,
+                    }
+                };
+            };
+            if rerun {
+                for (k, v) in scnd_run.iter() {
+                    for _ in 0..*v {
+                        match pos.add_rest(k.clone()) {
+                            Ok(_) => {},
+                            Err(_) => return Err(MoveError::ImpossiblePosition)
+                        }
+                    }
+                }
+            }
+            Ok(pos)
+
+        }
+
+        pub fn add_rest(&mut self, pce: Piece) -> Result<(usize, usize), CleaningError> {
+            match pce {
+                Piece::Queen(true) => {
+                    self.fields[7][0] = Piece::Queen(true);
+                    Ok((7, 0))
+                },
+                Piece::Queen(false) => {
+                    self.fields[0][13] = Piece::Queen(false);
+                    Ok((0, 13))
+                },
+                Piece::King(true) => {
+                    if !self.field_is_empty((7, 0)) {
+                        self.fields[7][1] = Piece::King(true);
+                        Ok((7, 1))
+                    } else {
+                        Err(CleaningError::KingBeforeQueen)
+                    }
+                },
+                Piece::King(false) => {
+                    if !self.field_is_empty((0, 13)) {
+                        self.fields[0][12] = Piece::King(false);
+                        Ok((0, 12))
+                    } else {
+                        Err(CleaningError::KingBeforeQueen)
+                    }
+                },
+                Piece::Rook(true) => {
+                    let (r, s) = (6, 0);
+                    if self.field_is_empty((r, s)) {
+                        self.fields[r][s] = Piece::Rook(true);
+                        Ok((r, s))
+                    } else {
+                        self.fields[r][s+1] = Piece::Rook(true);
+                        Ok((r, s+1))
+                    }
+                },
+                Piece::Rook(false) => {
+                    let (r, s) = (1, 13);
+                    if self.field_is_empty((r, s)) {
+                        self.fields[r][s] = Piece::Rook(false);
+                        Ok((r, s))
+                    } else {
+                        self.fields[r][s-1] = Piece::Rook(false);
+                        Ok((r, s-1))
+                    }
+                },
+                Piece::Knight(true) => {
+                    let (r, s) = (5, 0);
+                    if self.field_is_empty((r, s)) {
+                        self.fields[r][s] = Piece::Knight(true);
+                        Ok((r, s))
+                    } else {
+                        self.fields[r][s+1] = Piece::Knight(true);
+                        Ok((r, s+1))
+                    }
+                },
+                Piece::Knight(false) => {
+                    let (r, s) = (2, 13);
+                    if self.field_is_empty((r, s)) {
+                        self.fields[r][s] = Piece::Knight(false);
+                        Ok((r, s))
+                    } else {
+                        self.fields[r][s-1] = Piece::Knight(false);
+                        Ok((r, s-1))
+                    }
+                },
+                Piece::Bishop(true) => {
+                    let (r, s) = (4, 0);
+                    if self.field_is_empty((r, s)) {
+                        self.fields[r][s] = Piece::Bishop(true);
+                        Ok((r, s))
+                    } else {
+                        self.fields[r][s+1] = Piece::Bishop(true);
+                        Ok((r, s+1))
+                    }
+                },
+                Piece::Bishop(false) => {
+                    let (r, s) = (3, 13);
+                    if self.field_is_empty((r, s)) {
+                        self.fields[r][s] = Piece::Bishop(false);
+                        Ok((r, s))
+                    } else {
+                        self.fields[r][s-1] = Piece::Bishop(false);
+                        Ok((r, s-1))
+                    }
+                },
+                Piece::Pawn(true) => {
+                    for r in 0..4 {
+                        for s in 0..2 {
+                            if self.field_is_empty((r, s)) {
+                                self.fields[r][s] = Piece::Pawn(true);
+                                return Ok((r, s))
+                            }
+                        }
+                    };
+                    println!("here");
+                    Err(CleaningError::ImpossiblePosition)
+                },
+                Piece::Pawn(false) => {
+                    for r in 0..4 {
+                        for s in 0..2 {
+                            if self.field_is_empty((7-r, 13-s)) {
+                                self.fields[7-r][13-s] = Piece::Pawn(false);
+                                return Ok((7-r, 13-s))
+                            }
+                        }
+                    };
+                    println!("here");
+                    Err(CleaningError::ImpossiblePosition)
+                }
+                _ => return Err(CleaningError::KingBeforeQueen)
+            }
+        }
+
+        pub fn field_is_empty(&self, field: (usize, usize)) -> bool {
+            if self.fields[field.0][field.1] == Piece::None {
+                true
+            } else {
+                false
+            }
         }
 
         pub fn update(&mut self, cmove: &str) -> Result<(State, [[Piece; 14]; 8]), ModuleError> {
@@ -236,12 +465,12 @@ pub mod position {
 
         fn update_wo_capt(&mut self, piece: Piece, sf: &str, ef: &str) -> Result<(), MoveError> {
             self.update_ex_moves(piece, sf, ef);
-            let (mut lc, mut nc) = match self::Position::coordinates_to_index(ef) {
+            let (mut lc, mut nc) = match coordinates_to_index(ef) {
                 Ok(t) => t,
                 Err(rr) => return Err(rr)
             };
             self.fields[lc][nc] = piece;
-            (lc, nc) = match self::Position::coordinates_to_index(sf) {
+            (lc, nc) = match coordinates_to_index(sf) {
                 Ok(t) => t,
                 Err(rr) => return Err(rr)
             };
@@ -318,12 +547,11 @@ pub mod position {
                 },
                 (_, _) => {}
             }
-            match piece {
-                Piece::None => return Err(MoveError::NoFigurStart),
-                rp => {
-                    if self.colorw ^ rp.piece_to_color() {
-                        return Err(MoveError::WrongFigurStart)
-                    }
+            if piece == Piece::None {
+                return Err(MoveError::NoFigurStart);
+            } else {
+                if self.colorw ^ piece.piece_to_color() {
+                    return Err(MoveError::WrongFigurStart)
                 }
             };
             let capt_piece = match self.coordinates_to_piece(end_field) {
@@ -351,37 +579,32 @@ pub mod position {
         }
 
         pub fn coordinates_to_piece(&self, coordinate: &str) -> Result<Piece, MoveError> {
-            let (lett, num) = match self::Position::coordinates_to_index(coordinate) {
+            let (lett, num) = match coordinates_to_index(coordinate) {
                 Ok(a) => (a.0, a.1),
                 Err(rr) => return Err(rr)
             };
             Ok((&self.fields[lett][num]).clone())
-        }
-
-        pub fn coordinates_to_index(coordinate: &str) -> Result<(usize, usize), MoveError> {
-            let (letter, number) = coordinate.split_at(1);
-            let mut num: usize = match number.parse() {
-                Ok(n) => n,
-                Err(rr) => return Err(MoveError::MoveParse(rr))
-            };
-            match num {
-                1..9 => {},
-                _ => return Err(MoveError::UnrightCoordinates)
-            }
-            num = 8 - num;
-            let lett = letter.chars().map(|c| c as usize - 97).collect::<Vec<usize>>()[0];
-            match (num, lett) {
-                (0..8, 0..8) => {},
-                _ => return Err(MoveError::UnrightCoordinates)
-            };
-            Ok((num, lett))
-        }
-
-        
-
-        
+        }    
     }
 
+    pub fn coordinates_to_index(coordinate: &str) -> Result<(usize, usize), MoveError> {
+        let (letter, number) = coordinate.split_at(1);
+        let mut num: usize = match number.parse() {
+            Ok(n) => n,
+            Err(rr) => return Err(MoveError::MoveParse(rr))
+        };
+        match num {
+            1..9 => {},
+            _ => return Err(MoveError::UnrightCoordinates)
+        }
+        num = 8 - num;
+        let lett = letter.chars().map(|c| c as usize - 94).collect::<Vec<usize>>()[0];
+        match (num, lett) {
+            (0..8, 3..11) => {},
+            _ => return Err(MoveError::UnrightCoordinates)
+        };
+        Ok((num, lett))
+    }
 
     pub fn get_fen(position: &Position) -> String {
         let mut res = String::new();
@@ -391,7 +614,7 @@ pub mod position {
             let mut row = String::new();
             let lis = &position.fields[i];
             let mut empty_counter = 0;
-            for field in lis {
+            for field in &lis[3..11] {
                 match field {
                     Piece::None => {
                         empty_counter += 1;
@@ -440,7 +663,7 @@ pub mod position {
         res.push_str(&format!(" {}", position.since_pawn_major / 2));
 
         //movenumber
-        res.push_str(&format!(" {}", position.moves / 2));
+        res.push_str(&format!(" {}", (position.moves / 2) + 1));
 
 
         res
@@ -454,27 +677,13 @@ mod tests {
     use super::*;
 
     fn get_position() -> Position {
-        self::position::Position {
-            colorw: true,
-            fields: [[Piece::None,Piece::None,Piece::None,Piece::Rook(false),Piece::Knight(false),Piece::Bishop(false),Piece::Queen(false),Piece::King(false),Piece::Bishop(false),Piece::Knight(false),Piece::Rook(false),Piece::None,Piece::None,Piece::None],
-                [Piece::None,Piece::None,Piece::None,Piece::None,Piece::Pawn(false),Piece::None,Piece::None,Piece::Pawn(false),Piece::Pawn(false),Piece::Pawn(false),Piece::Pawn(false),Piece::None,Piece::None,Piece::None],
-                [Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None],
-                [Piece::None,Piece::None,Piece::None,Piece::Pawn(false),Piece::None,Piece::Pawn(false),Piece::Pawn(false),Piece::Pawn(true),Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None],
-                [Piece::None,Piece::None,Piece::None,Piece::Pawn(true),Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None],
-                [Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None,Piece::None],
-                [Piece::None,Piece::None,Piece::None,Piece::None,Piece::Pawn(true),Piece::Pawn(true),Piece::None,Piece::Pawn(true),Piece::Pawn(true),Piece::Pawn(true),Piece::Pawn(true),Piece::None,Piece::None,Piece::None],
-                [Piece::None,Piece::None,Piece::None,Piece::Rook(true),Piece::Knight(true),Piece::Bishop(true),Piece::Queen(true),Piece::King(true),Piece::Bishop(true),Piece::Knight(true),Piece::Rook(true),Piece::None,Piece::None,Piece::None]],
-            moves: 1,
-            en_passant: "d6".to_string(),
-            rochade: [Piece::King(true),Piece::Queen(true),Piece::King(false),Piece::Queen(false)],
-            since_pawn_major: 0
-        }
+        self::position::Position::from_fen("rnbqkbnr/1p2pppp/8/p1ppP3/P7/8/1PP1PPPP/RNBQKBNR w KQkq d6 0 6").unwrap()
     }
 
     #[test]
     fn it_works() {
         let result = position::get_fen(&get_position());
-        assert_eq!(result, String::from("rnbqkbnr/1p2pppp/8/p1ppP3/P7/8/1PP1PPPP/RNBQKBNR w KQkq d6 0 0"));
+        assert_eq!(result, String::from("rnbqkbnr/1p2pppp/8/p1ppP3/P7/8/1PP1PPPP/RNBQKBNR w KQkq d6 0 6"));
     }
 
     #[test]
@@ -491,17 +700,17 @@ mod tests {
 
     #[test]
     fn it_works3() {
-        let (a, b) = match  self::Position::coordinates_to_index("f7") {
+        let (a, b) = match  position::coordinates_to_index("f7") {
             Ok(b) => b,
             Err(_) => panic!()
         };
         assert_eq!(a, 1 as usize);
-        assert_eq!(b, 5 as usize);
+        assert_eq!(b, 8 as usize);
     }
 
     #[test]
     fn it_works4() {
-        let rr = match self::Position::coordinates_to_index("n9") {
+        let rr = match position::coordinates_to_index("n9") {
             Ok(_) => panic!(),
             Err(rr) => rr
         };
--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,9 +1,11 @@
+#![allow(unused_imports)]
 use mctrl::{motor::diagonal, *};
+use position::position;
 
 fn main() {
-    let mut m1 = motor::Mtr::new(true, 5, 6, 13).unwrap();
+    /*let mut m1 = motor::Mtr::new(true, 5, 6, 13).unwrap();
     m1.enable_motor();
-    m1.move_steps(120, true, 4.0).unwrap();
+    m1.move_steps(1600, true, 2.5).unwrap();
     delay::delayms(200);
     m1.move_steps(400, false, 4.0).unwrap();
     delay::delayms(200);
@@ -36,7 +38,10 @@ fn main() {
     let mut magnet = magnet::Magnet::new(26).unwrap();
     magnet.on();
     delay::delayms(7000);
-    magnet.off();
-
-
+    magnet.off();*/
+    std::env::set_var("RUST_BACKTRACE", "1");
+    let a = position::Position::from_fen(
+        "8/3R4/3kp3/1Q6/1P6/8/1PK1P3/8 b - - 8 44",
+    );
+    println!("{:?}", a);
 }
